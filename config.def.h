31408fd77dac53cd54573b92ae2fd15764c28c2f
Nathaniel Evan <nathanielevan@zohomail.com>
2021-03-13 05:32:08 +0700

    Applies patch slock-pam_auth-20190207-35633d4.diff

--- config.def.h
+++ config.def.h
@@ -6,7 +6,11 @@ static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[PAM] =    "#9400D3",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
