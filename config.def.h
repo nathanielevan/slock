5a20eb1a5248a28350376ee5e2350061c5a0b33e
Nathaniel Evan <nathanielevan@zohomail.com>
2021-03-14 02:02:08 +0700

    Defines user, group, message, colors etc

--- config.def.h
+++ config.def.h
@@ -1,28 +1,28 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "nate";
+static const char *group = "users";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
-	[PAM] =    "#9400D3",   /* waiting for PAM */
+	[INIT] =   "#2E3440",     /* after initialization */
+	[INPUT] =  "#5E81AC",   /* during input */
+	[FAILED] = "#BF616A",   /* wrong password */
+	[PAM] =    "#8FBCBB",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "Enter password to unlock.";
 
 /* text color */
-static const char * text_color = "#ffffff";
+static const char * text_color = "#d8dee9";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "6x13";
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 60;
 
 /* PAM service that's used for authentication */
 static const char* pam_service = "login";
